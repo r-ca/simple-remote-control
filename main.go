a44ea578516a72c2e568ddcdb5b125fc6346b926
rca <nem@nem.x0.to>
2024-11-06 17:01:01 +0900

    add cors rule

--- main.go
+++ main.go
@@ -145,6 +145,9 @@ func sendKeyEvent(key string) error {
 
 // Pingエンドポイントのハンドラー
 func pingHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	w.Write([]byte("pong"))
 	if pingLog {
 		logInfo("Pingを受信しました (Source: " + r.RemoteAddr + ")")
